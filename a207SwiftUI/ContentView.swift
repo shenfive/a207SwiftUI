b45a8833458dd8822bdec6cb19336d3325f24294
Danny Shen <shenfive@gmail.com>
2020-09-12 16:54:25 +0800

    Initial Commit

---
+++ a207SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  a207SwiftUI
+//
+//  Created by 申潤五 on 2020/9/12.
+//  Copyright © 2020 申潤五. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
